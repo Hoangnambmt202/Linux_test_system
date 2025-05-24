333c88983209142f5c2195a1c8d9545768d0f1dd
Hoang Nam <nam23062002@gmail.com>
2025-05-24 16:37:53 +0700

    add dj-database-url to requirements and update database settings for production

--- linux_test_system/settings.py
+++ linux_test_system/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,7 +28,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['.railway.app']
 
-
+SECRET_KEY = os.environ.get('SECRET_KEY', 'default-insecure-key-for-development')
 LOGIN_URL = '/login/'
 
 LOGOUT_REDIRECT_URL = '/login/'  # Sau khi đăng xuất, chuyển hướng về trang chủ
@@ -90,17 +90,22 @@ WSGI_APPLICATION = 'linux_test_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'linux_test_db',
+#         'USER': 'root', 
+#         'PASSWORD': '',   
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'linux_test_db',
-        'USER': 'root', 
-        'PASSWORD': '',   
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'), # Railway sẽ cung cấp biến này
+        conn_max_age=600 # Giữ kết nối alive trong 600 giây (10 phút)
+    )
 }
-
 
 
 # Password validation
