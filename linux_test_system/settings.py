d26fa1ddbf450a937f3d6ace2740d2e568efc379
Hoang Nam <nam23062002@gmail.com>
2025-05-25 17:55:52 +0700

    update ALLOWED_HOSTS and add CSRF_TRUSTED_ORIGINS for production environment

--- linux_test_system/settings.py
+++ linux_test_system/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+v@0g@=l-tyg)=43-elo#t8coy96-rob8#-v1%pjo(7^@a1qjg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*','.railway.app']
+ALLOWED_HOSTS = ['*',"web-production-68b65.up.railway.app",]
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'default-insecure-key-for-development')
 LOGIN_URL = '/login/'
@@ -144,6 +144,11 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+# settings.py
+CSRF_TRUSTED_ORIGINS = [
+    "https://web-production-68b65.up.railway.app",
+]
 
 
 # Static files (CSS, JavaScript, Images)
