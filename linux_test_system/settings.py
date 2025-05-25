7dc4f6451271c139b8858554cdaa461e855b62b9
Hoang Nam <nam23062002@gmail.com>
2025-05-26 00:31:10 +0700

    set DEBUG to True for development; add question images

--- linux_test_system/settings.py
+++ linux_test_system/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+v@0g@=l-tyg)=43-elo#t8coy96-rob8#-v1%pjo(7^@a1qjg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 LOGIN_URL = '/login/'
 
